75c6cd9b2ff8f9ab10a8e955ffee23b48f97ef4f
Lucas Felix <lfelixsampaio@gmail.com>
2017-09-19 01:25:02 +0000

    If/else

--- OlaMundo/OlaMundo/Form1.cs
+++ OlaMundo/OlaMundo/Form1.cs
@@ -24,15 +24,42 @@ namespace OlaMundo
             /*
              * variavel que guarda a idade
              */
-            int idade = 25;
+            int idade = 17;
             double altura = 1.8;
             float peso = 95.5f;
             bool ehFumante = true;
             char sexo = 'M';
 
-            decimal preco = 79.968464874m;
+            decimal preco = 79.9m;
 
-            MessageBox.Show($"Preço da academia: {preco:C}");
+            bool amigoDoDono = true;
+            if (idade >= 18 || amigoDoDono)
+            {
+                MessageBox.Show("Pode se matricular");
+            }
+            else
+            {
+                MessageBox.Show("Não pode ainda!");
+            }
+
+            // MessageBox.Show($"Preço da academia: {preco:C}");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int valor = 15;
+            string mensagem = "";
+            
+            if(valor > 10)
+            {
+                mensagem = "Maior que dez";
+            }
+            else
+            {
+                mensagem = "Menor que dez";
+            }
+
+            MessageBox.Show(mensagem);
         }
     }
 }
